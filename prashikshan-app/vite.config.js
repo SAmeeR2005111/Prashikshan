9f60987d4a829af42a1d843cfff845d0ddbf4dec
SAmeeR2005111 <sameerydv1920@gmail.com>
2025-10-14 17:47:55 +0530

    Fix

--- prashikshan-app/vite.config.js
+++ prashikshan-app/vite.config.js
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    open: true
+  base: process.env.VITE_BASE_PATH || "/Prashikshan"
   }
-})
+)
